2865ae4086362e6746369b6ddee0ff9d33d018c4
José Sánchez <jsanchez@turistik.com>
2025-02-19 17:41:19 -0300

    saludo

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -21,5 +21,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              #"Hola,Turistik"
+             # Hola omar
              status_code=200
         )
